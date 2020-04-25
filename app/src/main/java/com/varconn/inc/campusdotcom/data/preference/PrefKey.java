305883e265223f2c6515356bd9a121821daf89c9
dwaynetino <dwaynetino@gmail.com>
2020-04-25 20:16:49 +0200

    Initial commit

---
+++ app/src/main/java/com/varconn/inc/campusdotcom/data/preference/PrefKey.java
@@ -0,0 +1,7 @@
+package com.varconn.inc.campusdotcom.data.preference;
+
+public class PrefKey {
+    public static final String APP_PREF_NAME = "wpnews_app";
+    public static final String PREF_NAME = "name";
+    public static final String PREF_EMAIL = "email";
+}
